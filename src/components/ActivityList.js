389e4640cfe6785cba1d9a336854fd83ea7d6290
Fortune-Ndlovu <ndlovufortune97@gmail.com>
2024-10-29 00:30:25 +0000

    component to display activities with options to edit and delete

---
+++ src/components/ActivityList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ActivityList = ({activities, onEdit, onDelete}) => {
+    return (
+        <ul>
+            {activities.map((activity) => {
+                <li key={activity.id}>
+                    {activity.name} - {activity.duration} mins ({activity.type})
+                    <button onClick={() => onEdit(activity)}>Edit</button>
+                    <button onClick={() => onDelete(activity.id)}>Delete</button>
+                </li>
+            })}
+        </ul>
+    );
+};
+
+export default ActivityList;
